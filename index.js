4ae5188db617f055311a85424a14446c994d77c3
ElSnipe <el.snipe.7@gmail.com>
2024-10-07 06:59:33 +0300

    BURELIS

---
+++ index.js
@@ -0,0 +1,3 @@
+const Fruits = [{color: yellow, name:"pineapple"}, {color: yellow, name:"banana"}, {color:white, name:"coconot"}, {color:green, name:"cabbage"}]
+
+const yellowFruits = Fruits.filter(fruit => fruit.color === "yellow")
